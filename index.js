0a1f2f09d36fa0ba8c2df9726152089b0ffe04fe
Stephen Solka <solka@hey.com>
2021-03-20 11:16:28 -0400

    move more code to rust

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
-import { Notice, Plugin } from 'obsidian';
+import { Plugin } from 'obsidian';
 import rustPlugin from "./pkg/obsidian_rust_plugin_bg.wasm";
-import init from "./pkg/obsidian_rust_plugin.js";
+import * as plugin from "./pkg/obsidian_rust_plugin.js";
 
 export default class RustPlugin extends Plugin {
 	async onload() {
 		const buffer = Uint8Array.from(atob(rustPlugin), c => c.charCodeAt(0))
-		this.plugin = await init(Promise.resolve(buffer));
-		this.addCommand({
-			id: "example",
-			name: "Example",
-			callback: () => this.example()
-		});
-	}
-
-	example() {
-		new Notice(`rust says ${this.plugin.add(24, 24)}`)
+		await plugin.default(Promise.resolve(buffer));
+		plugin.onload(this);
 	}
 }
